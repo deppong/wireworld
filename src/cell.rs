82629d02f1b57997cae80bfec2164dea69914b07
Isaiah Deppong <isaiahjdeppong@gmail.com>
2023-09-22 18:01:00 -0400

    small adjustments for board struct basic layout

--- src/cell.rs
+++ src/cell.rs
@@ -1,3 +1,6 @@
+pub const BOARD_W: u32 = 800;
+pub const BOARD_H: u32 = 800;
+
 #[derive(Clone, Copy, Eq, PartialEq, Debug)]
 pub enum State {
     Empty,
@@ -20,5 +23,20 @@ impl State {
                 }
             },
         }
+    }
+}
+
+pub struct Board {
+    previous: [State; (BOARD_W*BOARD_H) as usize],
+    current: [State; (BOARD_W*BOARD_H) as usize]
+}
+
+impl Board {
+    pub fn get_previous(&self, x: u32, y: u32) -> State {
+        self.previous[(x + y * BOARD_W) as usize]
+    }
+
+    pub fn get_current(&self, x: u32, y: u32) -> State {
+        self.current[(x + y * BOARD_W) as usize]
     }
 }
